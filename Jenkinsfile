a327ae836a012707c70e20bec4fb30fe687d80e7
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-07-03 18:56:20 +0530

    feat: Create Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+@Library('pipeline-library-demo')_
+
+stage('Echo Hello') {
+    sayHello 'Jenkins'
+}
--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
+    // In here default value is set to 'human'
     echo "Hello, ${name}."
 }

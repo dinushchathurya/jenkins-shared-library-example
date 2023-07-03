94ee453ef62ebd24d02b94022473411ef01d631e
Dinush Chathurya <dinushchathurya21@gmail.com>
2023-07-03 19:47:14 +0530

    fix: Add missing closing bracket

--- vars/buildNodejs.groovy
+++ vars/buildNodejs.groovy
@@ -7,29 +7,31 @@ def call(String repoUrl, String branchName) {
         agent any
 
         stages { 
+
             stage("Checkout Code") {
                 steps {
                     git branch: "${branchName}", url: "${repoUrl}"
                 }
             }
-        }
-
-        stage("Build") {
-            steps {
-                sh "npm install"
+       
+            stage("Build") {
+                steps {
+                    sh "npm install"
+                }
             }
-        }
 
-        stage('Test') {
-            steps {
-                sh "npm test"
+            stage('Test') {
+                steps {
+                    sh "npm test"
+                }
             }
-        }
 
-        stage('Deploy') {
-            steps {
-                sh "npm run deploy"
+            stage('Deploy') {
+                steps {
+                    sh "npm run deploy"
+                }
             }
+            
         }
 
     }
